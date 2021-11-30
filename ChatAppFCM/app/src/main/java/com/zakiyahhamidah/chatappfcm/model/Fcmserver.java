74eb33853b2078a5e2044d6641ebd0ef97aeb314
kiyahza27 <56219654+kiyahza27@users.noreply.github.com>
2021-12-01 01:22:15 +0700

    Delete Fcmserver.java

--- ChatAppFCM/app/src/main/java/com/zakiyahhamidah/chatappfcm/model/Fcmserver.java
+++
@@ -1,9 +0,0 @@
-package com.zakiyahhamidah.chatappfcm.model;
-
-public class Fcmserver {
-
-        private String BASE_URL = "https://fcm.googleapis.com";
-        private String SERVER_KEY = "AAAAMhOAajc:APA91bHhdYMbey7VSxUWrjtL2vMTjorZyAwjzEwKOfR28j-xJpQK1qqMGI_9QUmR5Zc_tg4x958v6k9qOBHeQsXgkUiS8x_7aCSGecuFrAgYk8BpQozKLc1Xg6BqJNU4BEdBN0T0phkl";
-        private String CONTENT = "application/json";
-
-}
